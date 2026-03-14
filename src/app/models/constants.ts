7bdd319f6771455e1a97b0208f2b65609ce5adb7
Avinash <avinashmarbhal1994@gmail.com>
2026-03-14 14:18:24 +0530

    about page added

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,10 +1,10 @@
 export class Constants {
     public static APP_NAME1: string = 'Modern';
     public static APP_NAME2: string = 'Portfolio';
-    public static APP_NAME_COMBINE: string = this.APP_NAME1 + this.APP_NAME2;
+    public static APP_NAME_COMBINE: string = this.APP_NAME1 +'-'+ this.APP_NAME2;
     public static APP_NAME: string = this.APP_NAME1 + ' ' + this.APP_NAME2;
-    public static THEME_KEY: string = this.APP_NAME + '-' + 'theme'.toLowerCase();
-    public static SKIN_KEY: string = this.APP_NAME + '-' + 'skin'.toLowerCase();
+    public static THEME_KEY: string = (this.APP_NAME_COMBINE + '-' + 'theme').toLowerCase();
+    public static SKIN_KEY: string = (this.APP_NAME_COMBINE + '-' + 'skin').toLowerCase();
 
     // about me
     public static ABOUT_ME: any = {
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  
+  techStack = [
+    { name: 'Angular', icon: 'bi-patch-check', level: '95%' },
+    { name: 'TypeScript', icon: 'bi-code-slash', level: '90%' },
+    { name: 'Bootstrap 5', icon: 'bi-bootstrap', level: '95%' },
+    { name: 'Node.js', icon: 'bi-server', level: '80%' },
+    { name: 'Firebase', icon: 'bi-cloud', level: '85%' },
+    { name: 'UI/UX', icon: 'bi-palette', level: '88%' }
+  ];
 
+  education = [
+    { year: '2020 - 2024', degree: 'Bachelor of Computer Science', school: 'University Name' },
+    { year: '2018 - 2020', degree: 'Higher Secondary', school: 'School Name' }
+  ];
 }
