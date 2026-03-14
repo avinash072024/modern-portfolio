813c9ec9b39a498e8e34b0b101bb2d16d5160930
Avinash <avinashmarbhal1994@gmail.com>
2026-03-14 12:41:41 +0530

    foter chaged

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Constants } from '../../models/constants';
+import { AboutMe } from '../../interfaces/about-me';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink, TitleCasePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
+  myInformation: AboutMe = Constants.ABOUT_ME;
 }
--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -2,7 +2,21 @@ export class Constants {
     public static APP_NAME1: string = 'Modern';
     public static APP_NAME2: string = 'Portfolio';
     public static APP_NAME_COMBINE: string = this.APP_NAME1 + this.APP_NAME2;
-    public static APP_NAME: string = this.APP_NAME1 + '-' + this.APP_NAME2;
+    public static APP_NAME: string = this.APP_NAME1 + ' ' + this.APP_NAME2;
     public static THEME_KEY: string = this.APP_NAME + '-' + 'theme'.toLowerCase();
     public static SKIN_KEY: string = this.APP_NAME + '-' + 'skin'.toLowerCase();
+
+    // about me
+    public static ABOUT_ME: any = {
+        firstName: 'avinash', 
+        lastName: 'marbhal',
+        email: 'avinashmarbhal1994@outlook.com',
+        contact: '+91 87964 57407',
+        whatsapp: '+91 87964 57407',
+        linkedIn: 'https://www.linkedin.com/in/avinash-marbhal-808646264',
+        gitHub: 'https://github.com/avinash072024',
+        facebook: '',
+        instagram: 'https://www.instagram.com/avinash.marbhal.09',
+        location: 'Kolhapur, Maharashtra, India'
+    }
 }
