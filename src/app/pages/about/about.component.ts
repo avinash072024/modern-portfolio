4585eb352933763231fb26ffda3b15adb5212c3f
Avinash <avinashmarbhal1994@gmail.com>
2026-03-18 15:56:03 +0530

    added experiences & education

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 import { CtaComponent } from "../../components/cta/cta.component";
 import { RouterLink } from "@angular/router";
 
+export interface Experience {
+  id: number,
+  year: string,
+  designation: string,
+  company: string
+}
+
+export interface Education {
+  id: number,
+  year: string,
+  degree: string,
+  school: string
+}
+
 @Component({
   selector: 'app-about',
   imports: [CtaComponent, RouterLink],
@@ -19,8 +33,15 @@ export class AboutComponent {
     { name: 'UI/UX', icon: 'bi-palette', level: '88%' }
   ];
 
-  education = [
-    { year: '2020 - 2024', degree: 'Bachelor of Computer Science', school: 'University Name' },
-    { year: '2018 - 2020', degree: 'Higher Secondary', school: 'School Name' }
+  experience: Experience[] = [
+    { id: 1, year: '2021 - 2022', designation: 'Junior Web Developer', company: 'MindLine Technologies Pvt. Ltd.' },
+    { id: 2, year: '2022 - 2024', designation: 'Web Developer', company: 'Bluebenz Digitizations Pvt. Ltd.' },
+    { id: 3, year: '2024 - Present', designation: 'Senior Web Developer', company: 'Manorama Infosolutions Pvt. Ltd.' }
   ];
+
+  education: Education[] = [
+    { id: 1, year: '2011 - 2012', degree: 'Higher Secondary', school: 'Science Academy' },
+    { id: 2, year: '2012 - 2015', degree: 'Bachelor of Science', school: 'Shivaji University, Kolhapur' },
+    { id: 3, year: '2015 - 2018', degree: 'Master of Computer Application', school: 'Shivaji University, Kolhapur' }
+  ]
 }
