cdcae1dd2d5736fce964f5d5f9be9ff95ccd93ba
Avinash <avinashmarbhal1994@gmail.com>
2026-03-27 14:00:08 +0530

    services managed by dynamically

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CtaComponent } from "../../components/cta/cta.component";
+import { ServiceService } from '../../services/service/service.service';
+import { Service } from '../../interfaces/service';
 
 @Component({
   selector: 'app-services',
@@ -8,8 +10,29 @@ import { CtaComponent } from "../../components/cta/cta.component";
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
+  services: Service[] = [];
   router = inject(Router);
+  servicesService = inject(ServiceService);
+
+  ngOnInit(): void {
+    this.getServices();
+  }
+
+  getServices(): void {
+    this.servicesService.getServices().subscribe({
+      next: (res: any) => {
+        if (res.success) {
+          this.services = res?.services || [];
+        } else {
+          alert(res?.message);
+        }
+      },
+      error: (err: any) => {
+        alert(err.message);
+      },
+    })
+  }
 
   goToContact(): void {
     alert('Clicked');
