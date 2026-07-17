85a5e059af201bd82e68d0560a608c22843fa1b3
avinash072024 <www.avi.marbhal@gmail.com>
2026-07-17 20:41:45 +0530

    some essential changes done

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -38,7 +38,7 @@ export class FooterComponent implements OnInit {
 
   focusAreaBadges: dynamicObject[] = [
     { id: 1, name: 'Angular 14+', path: '' },
-    { id: 2, name: 'Bootstrap 5.3', path: '' },
+    { id: 2, name: 'Bootstrap 4+', path: '' },
     { id: 3, name: 'Performance', path: '' },
     { id: 4, name: 'Themeable UI', path: '' }
   ];
